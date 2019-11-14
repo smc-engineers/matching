fd67fee2e75cfd2e8c1e67e0e1b5b5ac1afd66b7
sukepii08 <sukepiisukagawa08@gmail.com>
2019-11-14 21:05:05 +0900

    create front screen by vue.js

---
+++ front/src-vue/js/index.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        organization: 0,
+        searchWord:"",
+        song_lists: []
+    },
+    
+    methods: {
+        onLoadSongs: function(){
+            $.ajax({
+                url : 'http://localhost:3000/api/v1/',
+                type : 'GET',
+                dataType: 'json',
+                data:{"word":this.searchWord}
+                //jsonp: 'callback',
+                //jsonpCallback: 'test_song_list'
+            }).done(function(data, textStatus, jqXHR){
+                console.log(data);
+                this.song_lists = data;
+            }.bind(this)).fail(function(jqXHR, textStatus, errorThrown){
+                console.log("通信失敗");
+            });
+        }
+        
+    }
+});
---
+++ front/src-vue/test_song_list.js
@@ -0,0 +1,29 @@
+test_song_list([
+    {
+        "name":"混声合唱(または重唱)とピアノのための「ヒスイ」",
+        "poetry":"寺山　修司",
+        "composition":"信長　貴富",
+        "organization":"混声",
+        "voice_organization":"SATB",
+        "accompaniment":"ピアノ伴奏",
+        "url":"https://www.panamusica.co.jp/ja/product/24011/"
+    },
+    {
+        "name":"無伴奏混声合唱のための「カウボーイ・ポップ」",
+        "poetry":"寺山　修司",
+        "composition":"信長　貴富",
+        "organization":"混声",
+        "voice_organization":"SATB div.",
+        "accompaniment":"アカペラ",
+        "url":"https://www.panamusica.co.jp/ja/product/7605"
+    },
+    {
+        "name":"無伴奏混声合唱のための「知るや君」",
+        "poetry":"島崎　藤村",
+        "composition":"相澤　直人",
+        "organization":"混声",
+        "voice_organization":"SATB div.",
+        "accompaniment":"アカペラ",
+        "url":"https://www.panamusica.co.jp/ja/product/23980/"
+    }
+]);
